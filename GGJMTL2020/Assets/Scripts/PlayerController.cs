0ff392b4a36ad4dd931f4445e449226446e347f4
Stephcraft <contact@stephcraft.net>
2020-02-01 11:54:49 -0500

    adapted player in test scene

--- GGJMTL2020/Assets/Scripts/PlayerController.cs
+++ GGJMTL2020/Assets/Scripts/PlayerController.cs
@@ -6,34 +6,49 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     Vector2 i_movement;
-    float moveSpeed = 5f;
+
+    public float moveSpeed = 5f;
+    public float jumpSpeed = 5f;
+
+    private float scale;
+
+    private Rigidbody2D rb2d;
     
     // Start is called before the first frame update
     void Start()
     {
-
+        rb2d = GetComponent<Rigidbody2D> ();
+        scale = transform.localScale.x;
     }
 
     // Update is called once per frame
     void Update()
+    {
+    }
+
+    void FixedUpdate()
     {
         Move();
     }
 
     private void Move()
     {
-        // (i_movement.x, 0, i_movement.y)
-        Vector3 movement = new Vector3(i_movement.x, 0, 0) * moveSpeed * Time.deltaTime;
-        transform.Translate(movement);
+        Vector2 movement = new Vector3(i_movement.x, 0) * moveSpeed;
+        rb2d.velocity = movement * Time.fixedDeltaTime;
     }
 
     private void OnMove(InputValue value)
     {
         i_movement = value.Get<Vector2>();
+
+        // flip
+        if(i_movement.x != 0)
+            transform.localScale = new Vector3(scale * Mathf.Sign(i_movement.x), transform.localScale.y, transform.localScale.z);
     }
 
-    private void OnMoveup()
+    private void OnMoveUp()
     {
-        transform.Translate(transform.up);
+        Vector2 movement = new Vector3(0, 1.0f) * jumpSpeed;
+        rb2d.velocity = new Vector2(rb2d.velocity.x, movement.y);
     }
 }
