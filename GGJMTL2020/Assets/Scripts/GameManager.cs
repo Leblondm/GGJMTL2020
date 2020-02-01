55c43acc5ba5229cb42276af227a901e31e83292
Stephcraft <contact@stephcraft.net>
2020-02-01 14:26:58 -0500

    ProgressBar

--- GGJMTL2020/Assets/Scripts/GameManager.cs
+++ GGJMTL2020/Assets/Scripts/GameManager.cs
@@ -2,17 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class Phase {
+    public float timing = 0f;
+    public float randomizedTiming = 0f;
+}
+
 public class GameManager : MonoBehaviour
 {
+    public float timing = 10f;
+    public float randomizedTiming = 2f;
+
+    public List<Phase> phases;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine("GameplayLoop");
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    IEnumerator GameplayLoop() {
+        while(true)
+        {
+            // TODO break something
+
+            float time = timing + Random.Range(-randomizedTiming, randomizedTiming);
+            yield return new WaitForSeconds(time);
+        }
     }
 }
---
+++ GGJMTL2020/Assets/Scripts/ProgressBar.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProgressBar : MonoBehaviour
+{
+    public float amount = 100;
+    public float progress = 100;
+
+    public Transform progressObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetProgress(progress);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public float GetPercentage() {
+        return progress / amount;
+    }
+
+    public void IncrementProgress(float value) {
+        SetProgress(progress + value);
+    }
+
+    public void ReduceProgress(float value) {
+        SetProgress(progress - value);
+    }
+
+    public void SetProgress(float value) {
+        progress = value;
+        progressObject.localScale = new Vector2(progress / amount, progressObject.localScale.y);
+    }
+}
