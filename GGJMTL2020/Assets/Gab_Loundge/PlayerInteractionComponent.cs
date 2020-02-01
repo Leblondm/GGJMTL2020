f2eeb85db8a0a589f6ac233ce09236c0ff36e792
Gabriel-PC <gab_sim16@hotmail.com>
2020-02-01 14:38:07 -0500

    Gabriel
    Add Dotween
    Interact'n shit
    ajout de layer

---
+++ GGJMTL2020/Assets/Gab_Loundge/PlayerInteractionComponent.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteractionComponent : MonoBehaviour
+{
+	public GameRessource currentStuff = GameRessource.None;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if(Input.GetButtonDown("Fire1")/*GetKeyDown(KeyCode.Space)*/ == true)
+		{
+			Collider2D[] interactInRange = Physics2D.OverlapBoxAll(transform.position, Vector2.one, 0, LayerMask.GetMask("Interact"));
+			if(interactInRange.Length > 0)
+			{
+				for (int i = 0; i < interactInRange.Length; i++)
+				{
+					Interactible myInteract = interactInRange[i].GetComponent<Interactible>();
+					if (myInteract != null)
+					{
+						myInteract.ExecuteInteract(this);
+						Debug.Log("Get GameObject = " + interactInRange[i].gameObject.name);
+					}
+				}
+				Debug.Log("Fin");
+			}
+		}
+    }
+}
